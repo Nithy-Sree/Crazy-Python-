4f9b5ca14af42975846b82e5b287b2141e763c75
Nithyasree <nithi0253@gmail.com>
2020-11-22 10:32:24 +0530

    Taking screenshot in python

--- ScreenShot.py
+++ ScreenShot.py
@@ -1,4 +1,9 @@
+# pip install PyAutoGUI
+
 import pyautogui
 
+# instance used to take screenshot using screenshot() class
 screenshot = pyautogui.screenshot()
+
+# saving the ss using save method of screenshot() class
 screenshot.save("screen.png")
