25cf2e1351e4e8da88b49e782d8a71d51e8ccd5a
Nithyasree <nithi0253@gmail.com>
2021-05-08 18:35:37 +0530

    System Details in Python

---
+++ systemDetails.py
@@ -0,0 +1,19 @@
+import platform
+
+# to know about the machine in your system
+print(platform.machine())
+
+# to know the name of your system
+print(platform.node())
+
+# to know which version of python you are using in your system
+print(platform.python_branch())
+
+# to know the architecture
+print(platform.architecture())
+
+# to know the version of windows
+print(platform.version())
+
+# all the details
+print(platform.uname())
